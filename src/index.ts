a4a474070e2dad9507b952d0fec0eecb84bfbe90
程崇臻 <15939054361@163.com>
2022-07-21 21:25:23 +0800

    Realize the monthDates method

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+type Week = Array<string | Date | number>
+
+export class Calendar {
+  firstWeekDay: number
+
+  constructor(firstWeekDay = 0) {
+    this.firstWeekDay = firstWeekDay
+  }
+
+  weekStartDate(date: Date): Date {
+    const starDate: Date = new Date(date.getTime())
+    while (starDate.getDate() !== this.firstWeekDay)
+      starDate.setDate(starDate.getDate() - 1)
+
+    return starDate
+  }
+
+  /**
+   * @description: 生成日历数组
+   * @param {number} year
+   * @param {number} month
+   * @param {function} dayFormatter
+   */
+  monthDates(year: number, month: number, dayFormatter: (d: Date) => string | Date | number): Array<Week> {
+    if (year < 1970)
+      console.warn('year must be a number >= 1970')
+
+    if ((month < 0) || (month > 11))
+      console.error('month must be a number (Jan is 0)')
+
+    let week: Week = []
+
+    const weeks: Array<Week> = []
+
+    let date = this.weekStartDate(new Date(year, month, 1))
+
+    do {
+      for (let i = 0; i < 7; i++) {
+        week.push(dayFormatter ? dayFormatter(date) : date)
+        date = new Date(date.getTime())
+        date.setDate(date.getDate() + 1)
+      }
+      weeks.push(week)
+      week = []
+    } while ((date.getMonth() <= month) && (date.getFullYear() === year))
+
+    return weeks
+  }
+}
+
